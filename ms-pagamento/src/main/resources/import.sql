8b123cc6018409a36aaa629fc435ed2d44a956b9
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-21 08:54:38 -0300

    feat: add import.sql

---
+++ ms-pagamento/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO  tb_pagamento(valor, nome, numero_do_cartao, validade, codigo_de_seguranca, status, pedido_id, forma_de_pagamento_id) VALUES (35.25,'Yasmin Chiquinha', '1234123412341234', '12/30', '123', 'CRIADO', 1, 2);
+INSERT INTO  tb_pagamento(valor, nome, numero_do_cartao, validade, codigo_de_seguranca, status, pedido_id, forma_de_pagamento_id) VALUES (99.25,'Fantine Chiquinha', '5678567856785678', '11/35', '153', 'CRIADO', 1, 2);
+INSERT INTO  tb_pagamento(valor, nome, numero_do_cartao, validade, codigo_de_seguranca, status, pedido_id, forma_de_pagamento_id) VALUES (55.78,'Ariadine Amaral', '1472147214721472', '12/35', '127', 'CRIADO', 1, 2);
