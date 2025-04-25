99c67915ec03a12eeb127d102344957ffea5715f
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-25 09:48:27 -0300

    feat: enum status

---
+++ ms-pedido/src/main/java/com/github/ariadineamaral/ms_pedido/entities/Status.java
@@ -0,0 +1,16 @@
+package com.github.ariadineamaral.ms_pedido.entities;
+
+public enum Status {
+
+    REALIZADO,
+    AGUARDANDO_PAGAMENTO,
+    PAGO,
+    CONFIRMADO,
+    PRONTO_PARA_ENTREGA,
+    SAIU_PARA_ENTREGA,
+    ENTREGUE,
+    CANCELADO
+
+    
+
+}
